7a4ce3842bd749f1c5a79ed29ede778cdc248735
Buddha <buddhagautam231@gmail.com>
2023-08-15 12:15:03 +0545

    feat: baseUrl points to hosted backEnd api

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8003/api/',
+    // baseURL: 'http://localhost:8000/api/',
+    baseURL: 'http://webapp231.pythonanywhere.com/api/',
 });
 
 export default axiosInstance;
